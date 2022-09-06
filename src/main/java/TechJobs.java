ee754764a1fc6b409c58beeddc56d16ba6496992
pharned <harned.exchange@gmail.com>
2022-09-06 00:02:46 -0500

    printjobs working I think

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -130,24 +130,22 @@ public class TechJobs {
         //so checking if the jobs exist in somejobs,
         // then using loops to scrounge through the arraylist and, in each array list, the hashmaps, to print the info
 
-        / Check if any jobs are in somejobs
         if (someJobs.size() > 0) {
-            //Using a loop to iterate over Array of HashMap Objects (Jobs)
-            for (HashMap<String, String> aJob : someJobs) {
+           for (HashMap<String, String> aJob : someJobs) {
                 System.out.println("\n*****");
-                // Then nest a for-each loop to iterate over the Hashmap Objects
+
                 for(Map.Entry<String, String> data: aJob.entrySet()) {
                     System.out.println(data.getKey() + ": " + data.getValue());
                 }
+
                 System.out.println("*****");
             }
         }
-        // Else if no jobs print "No Results"
         else {
-            // Use print instead of println so that a new line is not printed after No Results to match the test output
-            System.out.print("No Results");
+           System.out.print("No Results");
 
 
-        System.out.println("printJobs is not implemented yet");
+
     }
+}
 }
